168634ae09d05aecc4e18cca8d4ee5759fbcef94
Juan <you@example.com>
2022-03-04 22:10:04 -0500

    Configured toolbar with navigation component.

--- app/src/main/java/sanmi/labs/medialabandroidengineer/MainActivity.kt
+++ app/src/main/java/sanmi/labs/medialabandroidengineer/MainActivity.kt
@@ -2,10 +2,21 @@ package sanmi.labs.medialabandroidengineer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
     }
 }
