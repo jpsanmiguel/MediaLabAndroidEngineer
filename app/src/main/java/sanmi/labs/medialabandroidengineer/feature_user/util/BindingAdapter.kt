62df4508d90b0b5b6dab086816a24e8e1ba9d8ac
Juan <you@example.com>
2022-03-05 14:25:56 -0500

    Fixed bug sometime showing picture in ItemList that had no picture, but element before deleted had and takes that one.

--- app/src/main/java/sanmi/labs/medialabandroidengineer/feature_user/util/BindingAdapter.kt
+++ app/src/main/java/sanmi/labs/medialabandroidengineer/feature_user/util/BindingAdapter.kt
@@ -1,9 +1,11 @@
 package sanmi.labs.medialabandroidengineer.feature_user.util
 
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import sanmi.labs.medialabandroidengineer.R
 import sanmi.labs.medialabandroidengineer.feature_user.domain.model.User
 import sanmi.labs.medialabandroidengineer.feature_user.presentation.adapter.UserAdapter
 
@@ -17,5 +19,12 @@ fun bindUserListToRecyclerView(recyclerView: RecyclerView, userList: List<User>?
 fun bindImageUriStringToImageView(imageView: ImageView, imageUri: String) {
     if (imageUri.isNotBlank()) {
         imageView.setImageURI(imageUri.toUri())
+    } else {
+        imageView.setImageDrawable(
+            ContextCompat.getDrawable(
+                imageView.context,
+                R.drawable.ic_baseline_person_24
+            )
+        )
     }
 }
